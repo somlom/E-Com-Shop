a23f6861350bc33209781afdc9245ba1413d325b
supersnus <supersnus1331@gmail.com>
2022-11-17 17:08:46 +0100

    i18n implementation start

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static('./public'))
 
 app.get("/get_file", (req, res) => {
 
