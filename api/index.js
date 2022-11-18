076c29824d8054ffe87aaac14b21f57dbf07fe5b
supersnus <supersnus1331@gmail.com>
2022-11-18 22:05:56 +0100

    static files

--- api/index.js
+++ api/index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use("/products", products)
 
 app.get("/get_file", (req, res) => {
