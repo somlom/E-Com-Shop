9c46b356d48220ba395b273d3ea8a2f35c3272d8
supersnus <supersnus1331@gmail.com>
2022-11-23 13:42:40 +0100

    Modifications on cart, product

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -12,19 +12,23 @@ export function Cart() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
 
+  let total = 0;
   return (
     <div className='cart'>
 
       <span>Total {count.length} items</span>
-      <div>
-        {count.length === 0 ? <h1>No items here</h1> : count.map(data => (
-          <div className="product_in_cart" key={count.indexOf(data)}>
-            <button className='remove_item_button' onClick={() => dispatch(remove_from_cart(count.indexOf(data)))}>Remove item</button>
-            <p>{data.name}</p>
-            <p>price: {data.price}</p>
-            <button><Link to={`/products/${data.id}`}>Go to this element</Link></button>
-          </div>
-        ))}
+      {count.length === 0 ? <h1>No items here</h1> : count.map(data => (
+        total += data.price,
+        <div className="product_in_cart" key={count.indexOf(data)}>
+          <button className='remove_item_button' onClick={() => dispatch(remove_from_cart(count.indexOf(data)))}>Remove item</button>
+          <p>{data.name}</p>
+          <p>price: {data.price}</p>
+          <Link to={`/products/${data.id}`}>Go to this element</Link>
+        </div>
+      ))}
+      <div className='cart_footer'>
+        <p>Total: {total}</p>
+        <button>Checkout</button>
       </div>
 
     </div>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -8,9 +8,6 @@ export const PageNotFound = () => {
     return (
         <div>
             <h1 className='title'>¯\_(ツ)_/¯ PageNotFound</h1>
-            <Link to="/">
-                <button className='login_button opacity'> Go to home</button>
-            </Link>
             <div className='items_content'>
                 <h1 className='title'>Look at some new items</h1>
                 <Main />
