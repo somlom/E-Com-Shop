2ffefef600d22ec99cf82abe9eb1cf9ba33c5b59
G <trashcancereal@gmail.com>
2022-11-24 22:48:47 +0100

    - (+) ; (-) und remove item button verschönert

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -28,9 +28,9 @@ export function Cart() {
             <button className='remove_item_button' onClick={() => dispatch(remove_from_cart(count.indexOf(data)))}>Remove item</button>
 
             <div className='counter'>
-              <button className='remove_item_button' onClick={() => dispatch(add_to_cart(data))}>+</button>
+              <button className='increase_amount_button' onClick={() => dispatch(add_to_cart(data))}>+</button>
               <span>{data.count}</span>
-              <button className='remove_item_button' onClick={() => dispatch(remove_one_from_cart(data))}>-</button>
+              <button className='decrease_amount_button' onClick={() => dispatch(remove_one_from_cart(data))}>-</button>
             </div>
             <a href={`/products/${data.id}`}>Go to this element</a>
           </div>
