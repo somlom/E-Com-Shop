c4e4021007c524bc701ae56c455be636a4a72b91
Your Name <you@example.com>
2023-02-08 09:33:09 +0100

    stress test fix

--- src/Stresstest.jsx
+++ src/Stresstest.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Stresstest = () => {
   return (
-    require("./static/loaderio-9e785ff186e7e8db7b1d2e1b5a8ac1f2.txt")
+    "loaderio-9e785ff186e7e8db7b1d2e1b5a8ac1f2"
   )
 }
