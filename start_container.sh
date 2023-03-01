1cf91c5a2e992f2ec3bab09811e1e341e5f41804
Your Name <you@example.com>
2023-03-01 20:09:22 +0100

    docker script

--- start_container.sh
+++ start_container.sh
@@ -7,4 +7,5 @@ docker build -t test_container .
 echo Done!
 
 echo Starting container
-docker run -it --env-file=.env  -p 4000:4000 test_container
+docker run -d --env-file=.env -p 4000:4000 test_container
+# docker run -it -p 4000:4000 test_container
