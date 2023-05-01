a3582891bb1ddc372d0525a25cbf5e2b012a821f
Your Name <you@example.com>
2023-05-01 12:40:04 +0200

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 cd /home/dmitrii
-sudo ssh -i digital_ocean root@142.93.223.35 'cd /home/site && bash ./scripts/start_container.sh'
+sudo ssh -i digital_ocean root@204.48.22.191 'cd /home/site && bash ./scripts/start_container.sh'
 cd ~/Dokumente/code/site/api
 exit
--- web/src/i18n.js
+++ web/src/i18n.js
@@ -1,54 +1,25 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import XHR from "i18next-http-backend" // <---- add this
+import XHR from 'i18next-http-backend'; // <---- add this
 import Backend from 'i18next-http-backend';
 
-import DE from "./locales/de/translation.json";
-import EN from "./locales/en/translation.json";
-import ES from "./locales/es/translation.json";
-import FR from "./locales/fr/translation.json";
-import PL from "./locales/pl/translation.json";
-import RO from "./locales/ro/translation.json";
-import RU from "./locales/ru/translation.json";
-import TR from "./locales/tr/translation.json";
-import UA from "./locales/ua/translation.json";
-
+import DE from './locales/de/translation.json';
+import EN from './locales/en/translation.json';
 
 const resources = {
   en: {
-    translation: EN
+    translation: EN,
   },
   de: {
-    translation: DE
-  },
-  es: {
-    translation: ES
-  },
-  fr: {
-    translation: FR
-  },
-  ro: {
-    translation: RO
-  },
-  ru: {
-    translation: RU
-  },
-  ua: {
-    translation: UA
-  },
-  pl: {
-    translation: PL
-  },
-  tr: {
-    translation: TR
+    translation: DE,
   },
 };
 
 const options = {
   order: ['querystring', 'navigator'],
-  lookupQuerystring: 'lng'
-}
+  lookupQuerystring: 'lng',
+};
 i18n
   .use(Backend)
   .use(XHR) // <---- add this
@@ -58,14 +29,14 @@ i18n
     detection: options,
     resources,
     debug: true,
-    fallbackLng: "en",
+    fallbackLng: 'en',
     // supportedLngs: ['de', 'en', 'fr'],
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
     interpolation: {
-      escapeValue: false // xss safe
-    }
+      escapeValue: false, // xss safe
+    },
   });
 
 export default i18n;
