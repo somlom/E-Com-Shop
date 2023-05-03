872b40af51054bee0344fcbcd6fe49f10946747b
Your Name <you@example.com>
2023-05-03 12:24:40 +0200

    final check

--- scripts/copy_and_deploy.sh
+++ scripts/copy_and_deploy.sh
@@ -6,7 +6,7 @@
 # rm -rf node_modules build
  
 cd /home/dmitrii
-scp -r -i digital_ocean Dokumente/code/site root@204.48.22.191:/home
+scp -r -i digital_ocean Dokumente/code/site root@<your server's ip>:/home
 
 cd ~/Dokumente/code/site/api
 yarn install
