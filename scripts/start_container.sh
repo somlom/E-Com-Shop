74bc741d033f22513d196f464baf70652cd51a2a
Your Name <you@example.com>
2023-05-06 14:59:31 +0200

    scripts

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,10 +1,14 @@
 echo Cleaning old containers
+docker compose rm
 docker system prune -f
+docker system prune -a --volumes
+docker builder prune -a 
+docker buildx prune -a 
 echo Done!
 
 echo Starting container
 docker buildx bake
-docker-compose up --build
-docker-compose restart
+docker-compose up --build -d
+# docker-compose restart
 # docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d interecom.de
 docker ps
